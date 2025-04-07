e4548acd45d9201cf1a1b9decfbb94f1e2a47032
claramariano <claraamariano@gmail.com>
2025-04-07 08:44:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var doces = ["Chocolate", "Bala de goma", "Jujuba", "Pudim", 
+"Bolo de cenoura", "Sorvete", "Bombom", "Brigadeiro", "Beijinho",
+ "Casadinho", "Paçoca", "Açaí", "Trufa", "Doce de leite", "Nuttela", "Donuts"]
+
+ var doceEscolhido = parseInt (prompt("Digite um número de 0 a 15" ))
+
+ document.write ("Doce escolhido: " + doces[doceEscolhido])
